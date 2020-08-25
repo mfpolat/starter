dc42b041b68f0d805e93adb1aec85acfe96df5af
oak-mfpolat <mfatih.polat@oak.technolog>
2020-08-25 18:00:23 +0300

    no message

--- basestructure/src/main/java/com/mfpolat/starter/base/BaseListener.kt
+++ basestructure/src/main/java/com/mfpolat/starter/base/BaseListener.kt
@@ -2,22 +2,19 @@ package com.mfpolat.starter.base
 
 import android.util.Log
 import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
 
 interface BaseListener {
 
     fun listener(event: Any) {
 
     }
+
     fun disposableListener(log: String): Disposable {
         return EventBus.observe<Any>()
-            .subscribe({
-//                Log.d(log, it.toString())
-//                listener(it)
-            }, {
-//                Log.e("${log}_ERROR", it.localizedMessage, it)
-//                listener(it)
-            }, {
-
+            .subscribe(Consumer {
+                Log.d(log, it.toString())
+                listener(it)
             })
     }
 }
