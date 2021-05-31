2220749ea6f34f55a67cbf0d81ee59bca5fe166d
oak-mfpolat <mfatih.polat@oak.technolog>
2021-05-31 19:31:10 +0300

    no message

--- app/src/main/java/com/mfpolat/starterlibrarysample/MainActivity.kt
+++ app/src/main/java/com/mfpolat/starterlibrarysample/MainActivity.kt
@@ -2,11 +2,19 @@ package com.mfpolat.starterlibrarysample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mfpolat.starter.base.BaseListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,BaseListener{
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+
+    }
+
+    override fun listener(event: Any) {
+        super.listener(event)
     }
 }
